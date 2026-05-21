586a6bfd2e6e8b7a0402756c79c62d2064e5c3e9
shreyas9866 <shreyascheripalli@gmail.com>
2026-05-21 23:00:28 +0530

    fix import name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { TrainingProvider } from './context/TrainingContext'; // <-- IMPORT THIS
 import Dashboard from './pages/Dashboard';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import Navbar from './components/NavBar';
+import HomePage from './pages/HomePage';
 import History from './pages/History';
 import About from './pages/About';
 import Auth from './pages/Auth'; 
@@ -17,7 +17,7 @@ function App() {
           <div className="app-container">
             <Navbar />
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<HomePage />} />
               <Route path="/auth" element={<Auth />} />
               <Route path="/history" element={<History />} />
               <Route path="/about" element={<About />} />
